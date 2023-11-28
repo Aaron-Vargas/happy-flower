ce3a863f7fcf4120414f1863daaceb93d9433ea2
Aaron-Vargas <152305435+Aaron-Vargas@users.noreply.github.com>
2023-11-28 10:57:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ let mySprite = sprites.create(img`
     . . . . . 2 2 2 2 2 2 2 b . . . 
     . b b b b b 2 2 2 2 2 2 2 b . . 
     . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 c . . 
     . . b d 5 5 b 1 f f 5 4 4 c . . 
     b b d b 5 5 5 d f b 4 4 4 4 b . 
     b d d c d 5 5 b 5 4 4 4 4 4 4 b 
